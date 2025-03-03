f500da89cf8108bf9446946d536ca6bf8aec6b25
gabriellamsa <gabii.marcelly@gmail.com>
2025-03-03 16:57:49 -0500

    feat: add CartContext for state management and update product listing

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,7 +2,7 @@ export const CartItem = ({ item }) => {
   const { id, name, imageUrl, price } = item;
 
   return (
-    <div className="group flex flex-col gap-y-2 border border-zinc-200 rounded-md bg-white p-24">
+    <div className="group flex flex-col items-center gap-y-2 border border-zinc-200 rounded-md bg-white p-4">
       <img
         src={imageUrl}
         alt="Product Image"
@@ -10,6 +10,10 @@ export const CartItem = ({ item }) => {
         height={300}
         className="group-hover:-translate-y-2 transition-all duration-500"
       />
+      <div className="text-center">
+        <h1 className="text-zinc-700 text-sm">{name}</h1>
+        <span className="text-pink-400 text-sm">${price}</span>
+      </div>
     </div>
   );
 };
