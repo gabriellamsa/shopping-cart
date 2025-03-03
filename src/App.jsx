cf7037e7baf6c2b6961033b999672e60b93ed074
gabriellamsa <gabii.marcelly@gmail.com>
2025-03-03 16:38:25 -0500

    feat: add CartItem component and update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { allProducts } from "./assets/data/index";
+import { CartItem } from "./components/CartItem";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Shopping Cart</h1>;
+  return (
+    <div className="grid place-items-center py-20">
+      <h1 className="text-5xl italic text-gray-500 mb-16">
+        Trend Alert: Must-Have Outfits of the Season
+      </h1>
+      <div className="grid grid-cols-3 place-items-start gap-10">
+        {allProducts?.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
