a85e3e62068734a6012d4ce51b4a8589cdf29a83
gabriellamsa <gabii.marcelly@gmail.com>
2025-03-13 12:13:10 -0500

    feat: add cart quantity controls and remove button

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -6,13 +6,31 @@ export const CartButtons = ({ item }) => {
   return (
     <div className="w-max absolute right-5 top-5">
       <div className="space-x-3">
-        <button
-          type="button"
-          className="bg-zinc-400 border rounded-md px-2 py-1 text-sm text-white hover:bg-zinc-500 transition-colors"
-          onClick={() => addToCart(item)}
-        >
-          + Add to cart
-        </button>
+        {!item.inCart ? (
+          <button
+            type="button"
+            className="bg-zinc-400 border rounded-md px-2 py-1 text-sm text-white hover:bg-zinc-500 transition-colors"
+            onClick={() => addToCart(item)}
+          >
+            + Add to cart
+          </button>
+        ) : (
+          <div>
+            <div className="flex">
+              <button className="border rounded-lg px-3">-</button>
+              <p className="flex items-center gap-x-1 mx-1">
+                <span className="min-w-7 bg-green-100 grid place-items-center border rounded-full">
+                  1
+                </span>
+                <span className="text-sm">in cart</span>
+              </p>
+              <button className="border rounded-lg px-3">+</button>
+            </div>
+            <button className="bg-pink-300 mx-auto mt-2 block rounded-md px-2 py-1 text-xs text-white hover:bg-pink-400">
+              Remove
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
